5d489755939012f9930dbc419e752025f4d644df
Jason Brunelle <jason@jasonbrunelle.com>
2020-08-17 08:51:21 -0500

    Routing added

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent implements OnInit {
+  id: number;
+  recipe: Recipe;
+
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipes()[this.id];
+    });
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+}
